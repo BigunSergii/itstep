bb5d63ba663778b75dc5a255ea1c9aff29a0c630
root <root@localhost.localdomain>
2020-04-17 10:15:14 -0400

    change arr.py

--- arr.py
+++ arr.py
@@ -1,11 +1,11 @@
 from random import randint
 arra2 =[]
 y = 1
-n = 9
+n = 10
 d = 0
 while y <= n:
-    arra2.append(randint(-12, 90))
+    arra2.append(randint(-1, 90))
     y +=1
 c = sum(arra2)
 print (arra2, "Summa arra2 = ", c)
-print("hello")
+print("hello world")
