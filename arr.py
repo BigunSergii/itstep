bdebf508b2daeb09b9ed2b7c215e253aa398a8d3
SerhiiBihun <41513347+SerhiiBihun@users.noreply.github.com>
2020-04-17 17:26:01 +0300

    Update arr.py

--- arr.py
+++ arr.py
@@ -1,10 +1,10 @@
 from random import randint
 arra2 =[]
 y = 1
-n = 10
+n = 6
 d = 0
 while y <= n:
-    arra2.append(randint(-1, 90))
+    arra2.append(randint(-1, 800))
     y +=1
 c = sum(arra2)
 print (arra2, "Summa arra2 = ", c)
